277143463aa27d3cbdd931ded686538107492dbf
Alexander Shipin <ssanek212@gmail.com>
2022-11-25 22:49:16 +0100

    uncomment

--- Sources/PalindromGenrate/Command/8FCommand.swift
+++ Sources/PalindromGenrate/Command/8FCommand.swift
@@ -14,8 +14,8 @@ fileprivate let pop8FRegCommand = Command(
             .settings([.bigData, .bigAddress]),
             .nnn([(code: "0x00", formatter: BaseFormat { _ in
             """
-            // *target = *(uint%dataSize_t*)(mem(SR_SS) + reg_SP_%addressSizeu);
-            // reg_SP_%addressSizeu += %dataSize / 8;
+            *target = *(uint%dataSize_t*)(mem(SR_SS) + reg_SP_%addressSizeu);
+            reg_SP_%addressSizeu += %dataSize / 8;
             """
             })])
 
